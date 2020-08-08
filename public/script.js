4401b805f96e19035671269c9cd31bf1c8daba9d
Alexey Lysenko <detoner777@gmail.com>
2020-08-08 20:44:15 +0300

    add socket.io

--- public/script.js
+++ public/script.js
@@ -1,3 +1,4 @@
+const socket = io("/");
 const videoGrid = document.getElementById("video-grid");
 const myVideo = document.createElement("video");
 myVideo.muted = true;
@@ -12,6 +13,15 @@ navigator.mediaDevices
     myVideoStream = stream;
     addVideoStream(myVideo, stream);
   });
+
+socket.emit("join-room", ROOM_ID);
+socket.on("user-connected", () => {
+  connectToNewUser();
+});
+
+const connectToNewUser = () => {
+  console.log("new user");
+};
 
 const addVideoStream = (video, stream) => {
   video.srcObject = stream;
